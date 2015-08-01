e276e7bd58aefab2852d70ff48b3f4f4d9c93998
Sean O'Leary <sean@o-leary.co.nz>
2015-08-02 01:40:32 +1200

    Initial Commit

---
+++ googlewithcortana.js
@@ -0,0 +1,6 @@
+chrome.webRequest.onBeforeRequest.addListener(
+function(details){
+
+	return { redirectUrl: details.url.replace("www.bing.com/search", "www.google.com/search")};
+},
+ {urls: ["*://www.bing.com/search*"]}, ["blocking"]);
